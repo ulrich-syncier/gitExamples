ebbb2cd8ebf9114fc32cd29ae35fb074ad5822da
Ulrich Titz <ulrich.titz@syncier.com>
2021-01-18 10:50:54 +0100

    feat(main.py): Adjusted to use Dear instead of Hi.

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Ulrich')
+    print_hi('Ulrich Titz')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
