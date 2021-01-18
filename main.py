d27a47449d7066272159c55905a1452a2b2f72e5
Ulrich Titz <ulrich.titz@syncier.com>
2021-01-18 10:30:52 +0100

    feat(main.py): Adjusted to use Dear instead of Hi.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    print(f'Dear {name}')  # Press Strg+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
